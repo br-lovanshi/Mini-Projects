a123ad423adf8a9d7f415e9929852719448a0910
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2022-12-28 22:36:31 +0530

    Add files via upload

---
+++ springbootexceptionapp_282370/SpringBootexceptionhandling/src/main/java/com/masai/SpringBootexceptionhandlingApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootexceptionhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootexceptionhandlingApplication.class, args);
+	}
+
+}

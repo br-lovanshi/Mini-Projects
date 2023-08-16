5906bb462056294caad6f05d1e5a990d3dca3dc1
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-16 08:30:07 +0000

    repository

--- Inheritance-Mapping/src/main/java/com/Inheritance/Mapping/Repository/ContractEmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Inheritance.Mapping.Repository;
-
-import com.Inheritance.Mapping.Entity.ContractEmployee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContractEmployeeRepo extends JpaRepository<ContractEmployee, Integer> {
-}

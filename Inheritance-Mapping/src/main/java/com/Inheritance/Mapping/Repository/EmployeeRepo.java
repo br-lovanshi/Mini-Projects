4d667d28bd373ae5f45ce7614c71c8549aa1cb8d
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-08-16 08:30:25 +0000

    employe repo created

--- Inheritance-Mapping/src/main/java/com/Inheritance/Mapping/Repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Inheritance.Mapping.Repository;
-
-import com.Inheritance.Mapping.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-}

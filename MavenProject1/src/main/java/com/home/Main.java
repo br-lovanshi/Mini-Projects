b0cfe57bcaffcb1eec0b295629f6cc7e6e51b668
Brajesh Lovanshi <96133853+br-lovanshi@users.noreply.github.com>
2023-12-16 22:19:39 +0530

    Update Main.java

--- MavenProject1/src/main/java/com/home/Main.java
+++ MavenProject1/src/main/java/com/home/Main.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.Set;
 public class Main {
     public static void main(String[] args)  {
-            System.out.println("Hello");
             System.out.println("Welcome to the main page");
 
             StudentInterFace crud = new StudentService();
